5c1fffdda2270236a84dfd0cd2320dc615de6e76
divya-2599 <66492420+divya-2599@users.noreply.github.com>
2026-08-18 22:03:05 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n=s1.length(), m = s2.length(), count=0;
+        if(n>m)
+        return false;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(int i=0;i<n;i++) {
+            freq1[s1.charAt(i)-'a']++;
+            freq2[s2.charAt(i)-'a']++;
+        }
+        int matches=0;
+        for(int i=0;i<26;i++) {
+            if(freq1[i]==freq2[i])
+            matches++;
+        }
+        if(matches==26)
+        return true;
+        for(int i=n;i<m;i++) {
+            int add = s2.charAt(i)-'a';
+            if(freq1[add]==freq2[add]){
+               matches--;
+            }
+            freq2[add]++;
+            if(freq1[add]==freq2[add])
+            matches++;
+            int remove=s2.charAt(i-n)-'a';
+            if(freq2[remove]==freq1[remove])
+            matches--;
+            freq2[remove]--;
+            if(freq2[remove]==freq1[remove])
+            matches++;
+            if(matches==26)
+            return true;
+        }
+        return false;
+    }
+}
