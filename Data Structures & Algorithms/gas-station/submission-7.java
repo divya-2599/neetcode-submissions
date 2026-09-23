ab50c896198e78f82a8167e4d37de66602e9c028
divya-2599 <66492420+divya-2599@users.noreply.github.com>
2026-09-23 14:46:37 +0530

    Add: gas-station - submission-7

---
+++ Data Structures & Algorithms/gas-station/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum=0, index=0, maxm=Integer.MIN_VALUE, total=0;
+        for(int i=0;i<gas.length;i++) {
+            int cost1=gas[i]-cost[i];
+            sum+=cost1;
+            total+=cost1;
+            if(i<gas.length-1 && total<0){
+                total=0;
+                index=i+1;
+            }
+        }
+        if(sum>=0)
+        return index;
+        return -1;
+    }
+}
